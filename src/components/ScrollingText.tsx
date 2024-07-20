7129b4efd3f8429179869f1be27f7373bedb3f6e
Jude Joshua <officialuby@gmail.com>
2024-07-20 23:45:58 +0100

    Svg Imports trial fix 4

--- src/components/ScrollingText.tsx
+++ src/components/ScrollingText.tsx
@@ -8,7 +8,7 @@ interface ScrollingTextProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 interface ScrollingImagesProps extends React.HTMLAttributes<HTMLDivElement> {
-    images: React.FunctionComponent<React.SVGProps<SVGSVGElement>>[];
+    images: Array<React.FC<React.SVGProps<SVGSVGElement>>>;
     size: string;
 }
 
@@ -30,13 +30,16 @@ const ScrollingImages: React.FC<ScrollingImagesProps> = ({images, size, ...rest}
         return images.map((SrcComponent, index) => (
             <React.Fragment key={index}>
                 <SrcComponent/>
-                <IconComponent size={12} className={'text-card-area-icon'} weight={'fill'}/>
+                <IconComponent size={8} className={'text-card-area-icon'} weight={'fill'}/>
             </React.Fragment>
         ));
     };
     return (
         <div className={`scrolling-container ${size} flex items-center`}>
             <div className={'scrolling-text flex items-center justify-start'} {...rest}>
+                <div className={'scrolling-text-item flex flex-row items-center'}>
+                    {renderImages()}
+                </div>
                 <div className={'scrolling-text-item flex flex-row items-center'}>
                     {renderImages()}
                 </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from "vite-plugin-svgr"
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
+import dts from "vite-plugin-dts";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        react(),
-        svgr()
+        svgr(),
+        dts(),
+        react()
     ],
-})
+});
