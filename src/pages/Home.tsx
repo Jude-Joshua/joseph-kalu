7a78eb5252e23f71840d278205da83cc4313b2af
Jude Joshua <officialuby@gmail.com>
2024-07-20 22:52:46 +0100

    Svg Imports trial fix 3 - Now using Vite svgr import instead

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,21 +8,11 @@ import iconMapping from "../components/Icons.tsx";
 
 import HomeGlobe from "../assets/images/home-globe.webp";
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import {ReactComponent as Client01} from "../assets/images/clients/JNJ.svg";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import {ReactComponent as Client02} from "../assets/images/clients/meaningfulGigs.svg";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import {ReactComponent as Client03} from "../assets/images/clients/Designlab.svg";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import {ReactComponent as Client04} from "../assets/images/clients/KENVUE.svg";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import {ReactComponent as Client05} from "../assets/images/clients/adplist.svg";
+import Client01 from "../assets/images/clients/JNJ.svg?react";
+import Client02 from "../assets/images/clients/meaningfulGigs.svg?react";
+import Client03 from "../assets/images/clients/Designlab.svg?react";
+import Client04 from "../assets/images/clients/KENVUE.svg?react";
+import Client05 from "../assets/images/clients/adplist.svg?react";
 
 const roles = ["Design leader", "Design Strategist", "Community Builder"]
 const textCards = {
