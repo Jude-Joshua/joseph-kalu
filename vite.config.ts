2aa3726870bb6e1684f19e166ffc0496e32d259b
Jude Joshua <officialuby@gmail.com>
2024-07-20 23:55:57 +0100

    Remove d.ts to avoid issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
-import dts from "vite-plugin-dts";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
-        dts(),
         react(),
         svgr({
             // svgr options: https://react-svgr.com/docs/options/
