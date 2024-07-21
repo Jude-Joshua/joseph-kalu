0126a1ee34edead0334be80f61505bec49851cb1
Jude Joshua <officialuby@gmail.com>
2024-07-21 06:05:42 +0100

    Full homepage setup

--- src/components/ScrollingText.tsx
+++ src/components/ScrollingText.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import iconMapping from './Icons.tsx'
+import ImageComponent from "./ImageComponent.tsx";
 
 interface ScrollingTextProps extends React.HTMLAttributes<HTMLDivElement> {
     txt: string;
@@ -10,6 +11,7 @@ interface ScrollingTextProps extends React.HTMLAttributes<HTMLDivElement> {
 interface ScrollingImagesProps extends React.HTMLAttributes<HTMLDivElement> {
     images: string[];
     size: string;
+    type: string;
 }
 
 const ScrollingText: React.FC<ScrollingTextProps> = ({txt, size, emoji, ...rest}) => {
@@ -23,29 +25,41 @@ const ScrollingText: React.FC<ScrollingTextProps> = ({txt, size, emoji, ...rest}
     );
 };
 
-const ScrollingImages: React.FC<ScrollingImagesProps> = ({images, size, ...rest}) => {
+const ScrollingImages: React.FC<ScrollingImagesProps> = ({images, size, type, ...rest}) => {
     const IconComponent = iconMapping['Circle']
 
     const renderImages = () => {
-        return images.map((SrcComponent, index) => (
-            <React.Fragment key={index}>
-                <SrcComponent/>
-                <IconComponent size={8} className={'text-card-area-icon'} weight={'fill'}/>
-            </React.Fragment>
+        if (type === 'svg') {
+            return images.map((SrcComponent, index) => (
+                <React.Fragment key={index}>
+                    <SrcComponent/>
+                    <IconComponent size={8} className={'text-card-area-icon'} weight={'fill'}/>
+                </React.Fragment>
+            ));
+        }
+        if (type === 'image') {
+            return images.map((SrcComponent, index) => (
+                <React.Fragment key={index}>
+                    <ImageComponent source={SrcComponent} altText={`Number ${index+1} design exploration on Dribble`}/>
+                </React.Fragment>
+            ));
+        }
+        return null;
+    };
+
+    const renderDivs = () => {
+        const count = type === 'svg' ? 3 : type === 'image' ? 1 : 0;
+        return Array.from({ length: count }).map((_, index) => (
+            <div key={index} className={'scrolling-text-item flex flex-row items-center'}>
+                {renderImages()}
+            </div>
         ));
     };
+
     return (
         <div className={`scrolling-container ${size} flex items-center`}>
             <div className={'scrolling-text flex items-center justify-start'} {...rest}>
-                <div className={'scrolling-text-item flex flex-row items-center'}>
-                    {renderImages()}
-                </div>
-                <div className={'scrolling-text-item flex flex-row items-center'}>
-                    {renderImages()}
-                </div>
-                <div className={'scrolling-text-item flex flex-row items-center'}>
-                    {renderImages()}
-                </div>
+                {renderDivs()}
             </div>
         </div>
     );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import TextCard from "../components/TextCard.tsx";
 import ImageComponent from "../components/ImageComponent.tsx";
 import {ScrollingImages} from "../components/ScrollingText.tsx";
 import ProjectCard from "../components/ProjectCard.tsx";
+import Links from "../components/Links.tsx";
 import iconMapping from "../components/Icons.tsx";
 
 import HomeGlobe from "../assets/images/home-globe.webp";
@@ -13,6 +14,15 @@ import Client02 from "../assets/images/clients/meaningfulGigs.svg";
 import Client03 from "../assets/images/clients/Designlab.svg";
 import Client04 from "../assets/images/clients/KENVUE.svg";
 import Client05 from "../assets/images/clients/adplist.svg";
+// import Exploration01 from "../assets/images/exploration01.webp";
+import Exploration02 from "../assets/images/exploration02.webp";
+import Exploration03 from "../assets/images/exploration03.webp";
+import Exploration04 from "../assets/images/exploration04.webp";
+import Exploration05 from "../assets/images/exploration05.webp";
+import Exploration06 from "../assets/images/exploration06.webp";
+import Exploration07 from "../assets/images/exploration07.webp";
+import {ArrowLeft, ArrowRight} from "@phosphor-icons/react";
+import Reviewer01 from "../assets/images/erin.webp";
 
 const roles = ["Design leader", "Design Strategist", "Community Builder"]
 const textCards = {
@@ -22,7 +32,7 @@ const textCards = {
         "copyOne": 'The team at Kenvue is a mix of creativity, collaboration, and innovativeness over a cup of coffee.',
         "copyTwo": 'As the design leader, I harness each member\'s unique blend of ingenuity to drive the creation of our products.',
         "linker": 'Send me an email',
-        "linkLocation": '/'
+        "linkLocation": 'mailto:mrjosephkalu@gmail.com'
     },
     2: {
         "title": "Open source projects",
@@ -30,7 +40,7 @@ const textCards = {
         "copyOne": 'My weekends are spent on building free and useful projects for the community.',
         "copyTwo": 'I currently offer free resources on the Figma Community for designers, including icons, design systems, and plugins, to enhance their creative work.',
         "linker": 'Learn more about me',
-        "linkLocation": '/'
+        "linkLocation": '/about'
     },
     3: {
         "title": "Giving back to the community",
@@ -38,10 +48,11 @@ const textCards = {
         "copyOne": '“We are not put on this earth for ourselves, but are placed here for each other. If you are there always for others, then in time of need, someone will be there for you.” Jeff Warner',
         "copyTwo": 'I mentor and build tech communities.',
         "linker": 'Discover my impact',
-        "linkLocation": '/'
+        "linkLocation": '/talks'
     },
 }
 const clientLogos = [Client01, Client02, Client03, Client04, Client05]
+const designExplorations = [Exploration02, Exploration03, Exploration04, Exploration05, Exploration06, Exploration07]
 const sampleProject = {
     1: {
         "title": "Helping smokers combat cigarette cravings",
@@ -107,8 +118,8 @@ const Home: React.FC = () => {
             </header>
             <section className={'flex flex-col items-start justify-start'}>
                 <article className={'clients'}>
-                    <ScrollingImages images={clientLogos} size={'full'}/>
-                    <ScrollingImages images={clientLogos} size={'full'}/>
+                    <ScrollingImages images={clientLogos} size={'full'} type={'svg'}/>
+                    <ScrollingImages images={clientLogos} size={'full'} type={'svg'}/>
                 </article>
                 <article className={'caseStudies'}>
                     <div className={'caseStudies-holder flex flex-col'}>
@@ -122,8 +133,161 @@ const Home: React.FC = () => {
                         ))}
                     </div>
                 </article>
-
-
+                <PageDivider/>
+                <article className={'testimonials flex flex-col'}>
+                    <div className="article-title">
+                        <div className="article-title-text flex flex-col">
+                            <h3 className={'h3'}>Creating impact since 2019. These are the <span
+                                id={'highlight'}>numbers</span></h3>
+                        </div>
+                    </div>
+                    <PageDivider/>
+                    <div className="facts-container flex flex-col">
+                        <div className="facts-container-fact flex flex-row justify-between items-start">
+                            <p className="h6">
+                                5 years pioneering unique solutions
+                            </p>
+                            <p className="p1">
+                                5+ years of working with dynamic teams to transform ideas into great products.
+                            </p>
+                        </div>
+                        <PageDivider/>
+                        <div className="facts-container-fact flex flex-row justify-between items-start">
+                            <p className="h6">
+                                255+ mentees worldwide
+                            </p>
+                            <p className="p1">
+                                <span>With a worldwide collage of impactful mentorship, I’ve guided designers from clueless to experts in their crafts. I mentor designers from <Links
+                                    classes={'p1'} to={'https://designlab.com/'}
+                                    border={true}>Designlab</Links> to <Links classes={''} to={'https://adplist.com/'}
+                                                                              border={true}>ADPList</Links>.</span>
+                            </p>
+                        </div>
+                        <PageDivider/>
+                        <div className="facts-container-fact flex flex-row justify-between items-start">
+                            <p className="h6">
+                                5 community contributions
+                            </p>
+                            <p className="p1">
+                                From open source to community building, I’ve given back projects to the
+                                community.
+                            </p>
+                        </div>
+                        <PageDivider/>
+                        <div className="facts-container-fact flex flex-row justify-between items-start">
+                            <p className="h6">
+                                45+ completed projects
+                            </p>
+                            <p className="p1">
+                                Led a design team of five and another team of three to deliver innovative
+                                projects. Additionally, I've handled a substantial amount of freelance work for
+                                various clients.
+                            </p>
+                        </div>
+                    </div>
+                    <PageDivider/>
+                    <div className="review">
+                        <div className="review-testimonial flex flex-row items-center justify-center">
+                            <div className="button-icon">
+                                <ArrowLeft size={'24'} weight={'regular'}/>
+                            </div>
+                            <div className="review-testimonial-person flex flex-col justify-center items-center">
+                                <ImageComponent altText={'first Reviewer, Erin Skurdal'} source={Reviewer01}/>
+                                <div className="review-testimonial-person-details flex flex-col">
+                                    <p className="h6 text-center testimonial-person-details-name">
+                                        Erin Skurdal
+                                    </p>
+                                    <p className="c1 text-center review-testimonial-person-details-position">
+                                        Director, Global UX Design, Kenvue Inc.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="review-testimonial-review">
+                                <p className="p1">
+                                    Joseph was a member of my global UX design team for 18 months. During
+                                    that time, I was grateful for the thoughtfulness he brought to every
+                                    design challenge and his positive attitude which continuously elevated
+                                    the results of the cross functional team he worked with. Joseph brought
+                                    both a consumer and business-savvy mindset to his design decisions. He
+                                    is committed to creating and leveraging design systems to drive
+                                    consistent and efficient digital experiences. I hope I am able to hire
+                                    Joseph again in the future. He will be missed until then.
+                                </p>
+                            </div>
+                            <div className="button-icon right">
+                                <ArrowRight size={'24'} weight={'regular'}/>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <PageDivider/>
+                <article className={'brief flex flex-col'}>
+                    <div className="article-title flex flex-col">
+                        <div className="article-title-text flex flex-col">
+                            <h3 className={'h3'}>Beyond the design desk...</h3>
+                        </div>
+                        <div className="article-title-text flex flex-row justify-end items-end">
+                            <h3 className={'h3 text-right'}><span
+                                id={'highlight'}>(when I’m not in front of pc screens)</span></h3>
+                        </div>
+                    </div>
+                    <PageDivider/>
+                    <div className="facts-container flex flex-col">
+                        <div className="facts-container-fact flex flex-row justify-between items-start">
+                            <p className="h6">
+                                Participating in design panels and talks
+                            </p>
+                            <p className="p1">
+                                <Links to={'#'} classes={''} border={true}>GOMYCODE</Links> | <Links to={'#'}
+                                                                                                     classes={''}
+                                                                                                     border={true}>KOM</Links> | <Links
+                                to={'#'} classes={''} border={true}>Abia Tech Flock</Links>
+                            </p>
+                        </div>
+                        <PageDivider/>
+                        <div className="facts-container-fact flex flex-row justify-between items-start">
+                            <p className="h6">
+                                Writing think pieces
+                            </p>
+                            <p className="p1">
+                                <Links to={'#'} classes={''} border={true}>The Nation Newspaper</Links>
+                            </p>
+                        </div>
+                        <PageDivider/>
+                        <div className="facts-container-fact flex flex-row justify-between items-start">
+                            <p className="h6">
+                                Collaborating on open-source community projects
+                            </p>
+                            <p className="p1">
+                                <Links to={'#'} classes={''} border={true}>Memoji</Links> | <Links to={'#'} classes={''}
+                                                                                                   border={true}>Nebulon</Links>
+                            </p>
+                        </div>
+                        <PageDivider/>
+                        <div className="facts-container-fact flex flex-row justify-between items-start">
+                            <p className="h6">
+                                Mentoring new and aspiring designers
+                            </p>
+                            <p className="p1">
+                                <Links to={'#'} classes={''} border={true}>Designlab</Links> | <Links to={'#'}
+                                                                                                      classes={''}
+                                                                                                      border={true}>ADPList</Links>
+                            </p>
+                        </div>
+                        <PageDivider/>
+                        <div className="facts-container-fact flex flex-row justify-between items-start">
+                            <p className="h6">
+                                Hosting design workshops
+                            </p>
+                            <p className="p1">
+                                <Links to={'#'} classes={''} border={true}>WCAG 2.0 and UX</Links>
+                            </p>
+                        </div>
+                    </div>
+                </article>
+                <article className={'explorations'}>
+                    <ScrollingImages images={designExplorations} size={'full'} type={'image'}/>
+                </article>
                 <PageDivider/>
             </section>
         </>
