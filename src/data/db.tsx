426b2b20ec35e77e67453c648a8732f26c2c12d1
Jude Joshua <officialuby@gmail.com>
2024-07-26 06:57:38 +0100

    accorn project work

--- src/data/db.tsx
+++ src/data/db.tsx
@@ -35,6 +35,7 @@ const Data = {
             "year": "2023",
             "industry": "Healthcare, SaaS",
             "password": true
+
         },
         4: {
             "id": 4,
@@ -163,6 +164,7 @@ const Data = {
                 "image": [
                     "old-app.webp",
                 ]
+
             },
             "style guide": {
                 "desc": "To keep the visual elements appealing and delightful, it was crucial to create a set of rules and guidelines that is clear and intuitive for end users. These includes customizable UI components, identifiable brand colors, clear CTAs, etc. Typography should reflect simplicity, readability and how easily it is for users to find content within the product.",
@@ -184,12 +186,191 @@ const Data = {
                     "testing.webp",
                 ]
             },
-            "success metrics": "During the stakeholders engagement meetings, it was clear that the goal of the business is to increase user engagement and retention. We aim to boost the number of daily active users by 20% within the first six months and ensure an average session duration of at least 10 minutes. We also aim to retain 60% of new users after three months and achieve at least a 4.5-star rating on app stores.",
-            "results": "At the end of this project, a well-researched and user-friendly app emerged. The new design successfully addressed the challenges identified in the initial stages. Our success metrics were met, with a significant boost in user engagement and retention. The app's daily active users increased by 25% within the first six months, and the average session duration exceeded our goal, reaching 12 minutes. Additionally, the app retained 65% of new users after three months and achieved a 4.6-star rating on app stores, surpassing our initial targets.",
-            "challenges": "The major challenge was the users' resistance to changing their habits. While the new features and design aimed to provide support and motivation, convincing users to consistently use the app and engage with its features was difficult.",
-            "conclusion": "Redesigning the smoking cessation app and adding new features was a challenging but rewarding project. The new design and features successfully addressed the initial challenges and met the project's goals, leading to increased user engagement and retention. The app now provides a more effective and enjoyable experience for users, helping them quit smoking and improve their health.",
+            "success metrics": "During the stakeholders engagement meetings, it was clear that the goal of the business is to increase user engagement and retention. We aim to boost the number of daily active users by 20% within the first six months and ensure an average session duration of at least 10 minutes. We also aim to retain 60% of new users after three months and reduce the user churn rate by 15% within the first year.\n\nWe want the new features to be valuable and helpful. The target is for 60% of users to participate in the support community, and 50% to schedule coaching sessions with healthcare professionals.\n\nUser satisfaction is crucial. We aim to achieve an average user satisfaction rating of 4.5 out of 5 in post-task surveys and receive positive feedback in at least 80% of user reviews.\n\nFinally, we measure success by the app's impact on users' smoking habits. Our goal is for 60% of users to reduce their smoking frequency by 50% within three months and for 30% of users to report quitting smoking entirely within six months.",
+            "results": {
+                "desc": "The product was launched in 3 markets in Europe and the United States with a total of 30k app download and a retention rate of 70%. The design were implemented in iOS and Android.",
+                "image": [
+                    "results.webp",
+                ]
+            },
+            "challenges": "The main challenges was the regulatory requirement for the EU market. Designing a health app for 4 different markets was a very huge task with many regulatory framework to deal with. But we were able to find common ground between UX, business and regulator.",
+            "conclusion": "Rebranding the smoking cessation app involved several challenges, including aligning with the company's brand, balancing user preferences, redesigning the interface, adding new features, and improving navigation. Despite these hurdles, the project successfully delivered a more visually appealing, user-friendly, and effective app to help users quit smoking.\n\nIt was also a good experience creating a documentation for the developers to follow while building the app. We did UAT Testing to ensure what was designed is what has been developed.\n\nOverall, rebranding this app was a delightful experience."
+
         },
-    }
+        4: {
+            "projectName": "Manilla Crypto",
+            "context": "",
+            "problem statement": "",
+            "crafted solution": "",
+            "the story": "",
+            "the challenge": "",
+            "project goal": "",
+            "the process": "",
+            "market research": "",
+            "analysis": "",
+            "brainstorming and ideation": "",
+            "develop": "",
+            "information architecture": "",
+            "the old app": "",
+            "sketches and wireframes": "",
+            "style guide": "",
+            "final designs": "",
+            "user testing": "",
+            "success metrics": "",
+            "results": "",
+            "challenges": "",
+            "conclusion": ""
+        },
+        5: {
+            "projectName": "HumanManager",
+            "context": "",
+            "problem statement": "",
+            "crafted solution": "",
+            "the story": "",
+            "the challenge": "",
+            "project goal": "",
+            "the process": "",
+            "market research": "",
+            "analysis": "",
+            "brainstorming and ideation": "",
+            "develop": "",
+            "information architecture": "",
+            "the old app": "",
+            "sketches and wireframes": "",
+            "style guide": "",
+            "final designs": "",
+            "user testing": "",
+            "success metrics": "",
+            "results": "",
+            "challenges": "",
+            "conclusion": ""
+        },
+        6: {
+            "projectName": "NFT Marketplace Design",
+            "context": "With the rise of the NFT boom in 2021, many startups were founded, and exchanges began building NFT platforms for their users. When our company launched its cryptocurrency exchange, we realized that NFTs presented another goldmine we could tap into. This recognition led us to the need to build an NFT platform where users could mint and trade NFTs.\n\nWe immediately went to work to understand users' needs, the market landscape, and what competitors were building. After extensive research, we developed flows and designs that would be intuitive and user-friendly. This strategic approach ensured that our platform not only met market demands but also provided a seamless and engaging experience for our users.",
+            "problem statement": "In 2021, NFTs became very popular, and many platforms were created for minting and trading them. However, many of these platforms were hard to use and confusing. Our company saw an opportunity with NFTs but needed to build a platform that was easy to use and competitive.We needed to develop a solution that would simplify the process of minting and trading NFTs and make it fun for people to mint and trade NFTs, while setting ourselves apart from existing competitors.",
+            "crafted solution": {
+                "desc": "After thorough research and multiple iterations, we arrived at the final design for our NFT platform. Our primary focus was on simplicity and ease of use.",
+                "image": [
+                    "solution.webp",
+                ]
+            },
+            "the story": {
+                "image": [
+                    "story01.webp",
+                ]
+            },
+            "project goal": "Our goal for this project is to create an easy-to-use NFT platform where people can mint and trade NFTs. We aim to make the process simple and intuitive for categories of users, whether they are new to NFTs or have a ton of experience. We want our platform to be secure, reliable, and user-friendly, standing out from other platforms in the market.",
+            "market research": {
+                "desc": "To get things started, I conducted a preliminary market research to gain a comprehensive understanding of the NFT and crypto currency landscape. This research encompasses market size, user demographic, competitor analysis, market trends, technological landscape and  regulations.",
+                "image": [
+                    "styleguide.webp",
+                    "styleguide.webp",
+                    "styleguide.webp",
+                ]
+            },
+            "analysis": {
+                "desc": "We analysed all the collected insights from the user interview and survey and arranged into an affinity map so we can come up with some valuable information that could lead to a possible solution.\n\nBased on the affinity map, we were able to come up with six pain points that our user persona was experiencing and needed a solution to.\n\nWe understood that addressing these pain points will not only make our product stand out, it’ll also attract users to be fully engaged in using the product and in-turn increase referrals.",
+                "image": [
+                    "solution.webp",
+                ]
+            },
+            "brainstorming and ideation": {
+                "image": [
+                    "solution.webp",
+                    "solution.webp",
+                ]
+            },
+            "develop": {
+                "image": [
+                    "solution.webp",
+                    "solution.webp",
+                ]
+            },
+            "sketches and wireframes": {
+                "desc": "We used wireframe to quickly test the ideas and iterate without much effort and finance. We started by sketching out the wireframe, tested it in house, iterated and tested it with potential users. At the end of the iterative process, we adjusted our designs and came with a better product design. ",
+                "image": [
+                    "solution.webp",
+                ]
+            },
+            "style guide": {
+                "desc": "We created a style guide to enable us have a consistent design with the crypto exchange and make design and development efforts less stressful.",
+                "image": [
+                    "styleguide.webp",
+                ]
+            },
+            "final designs": {
+                "image": [
+                    "final01.webp",
+                ]
+            }
+            ,
+            "user testing": {
+                "desc": "To check how easy and effective the NFT platform we’ve designed is to use. We conducted a user testing before going into development and further release into the market.",
+                "image": [
+                    "testing.webp",
+                ]
+            },
+            "success metrics": "We wanted to measure the success of the NFT platform over time to make sure it meets user needs and also profitable for the business. To validate this, we focused on key metrics that are important success indicators.\n\nOne of the metrics we tracked was the percentage of users who complete onboarding, mint an NFT and list it for sale. We also tracked the time it took to mint an NFT. The goal is to know if the issue is a design or technical problem.\n\nWe tracked net promoter score which is the willingness of a user to recommend the platform. Since we have a referral program, it was easy to input who referred who. Finally, we tracked user retention rate and new user growth rate. One of  the success of a product is on how many people return to use it and how fast new users get to discover the platform.",
+            "results": {
+                "desc": "The results are a landing page, a web app (Web, iPad and Mobile) and an academy where people learn about NFT",
+                "image": [
+                    "results.webp",
+                ]
+            },
+            "challenges": {
+                "desc": "While designing this product, we were faced several challenges that required us to be flexible and go back to the drawing board. We couldn’t skip or ignore some of these challenges so we don’t waste time and resource only to hit it in the future.",
+                "parts": {
+                    "": "The first challenge was the complex industry we were building for. We needed to comply with a lot of regulations as the company is based in Europe",
+                    "  ": "Another challenge is the technicality of building a platform that can mint NFT cross chain. We wanted to ensure our users can mint on Solana and other platforms that weren’t as expensive as Ethereum.",
+                },
+                "close": "Despite these challenges, we were able to overcome them using design thinking methodology. By remaining focused on the users and testing and iterating regularly we were able to design a platform that was very easy for the user to navigate.",
+            },
+            "conclusion": "This product design case study highlights the success of using the design thinking methodology to design products. By focusing on the needs and desires of users, we were able to create a product that not only addressed their pain points but also exceeded their expectations.\n\nThe success of this project is a testament to the power of design thinking and its ability to drive innovation and improve products. By adopting this approach, we were able to create a product that not only met the needs of our users but also differentiated us from our competitors in the market.",
+        },
+        7: {
+            "projectName": "Accorn",
+            "context": "In the ever-evolving landscape of Nigerian banking, the persistent challenge of financial exclusion looms large. With millions of individuals left unbanked or underbanked, the need for innovative solutions has never been more pressing. This case study delves into the journey of Accorn - Agency Banking, a project aimed at leveraging technology to bring basic banking services to remote and underserved communities.\n\nAccorn utilizes a network of local merchants, in areas where traditional banking usually doesn’t have a presence, to provide basic banking services. The agent is equipped with basic knowledge of the application and they get to provide banking services to the rural populace with just their mobile application.",
+            "problem statement": {
+                "desc": "Before delving into design solutions, it was crucial to grasp the nuances of the Nigerian banking ecosystem. Through  market research, I uncovered startling statistics.\n\nAccording to the Business Day financial inclusion  report  of December 2021, 36% of Nigeria’s 106 million adults (18 years and older) are still entirely financially excluded, or 38.1 million people remained financially excluded.\n\nTraditional banking institutions, largely concentrated in urban areas, failed to reach the majority of the population, especially in rural regions.\n\nPeople are unbanked for a variety of reasons.",
+                "image": [
+                    "testing.webp",
+                ],
+                "parts": "Main reasons for being unbanked\n\nPoor credit history\n\n Lack of trust in mainstream banking\n\nProblems with language and literacy\n\nUnemployed or lack steady income\n\nMajor causes include"
+            },
+            "Identifying Problems": "The core challenge lay in accessibility. How could we empower individuals in remote areas to access basic financial services conveniently and securely? Traditional brick-and-mortar banks have been impractical, leaving a segment of the population without viable options.\n\nThe task is clear: design a solution that bridged the gap between underserved communities and essential banking services.",
+            "user flows": {
+                "image": [
+                    "solution.webp",
+                    "solution.webp",
+                ]
+            },
+            "wireframes": {
+                "desc": "Creating a wireframe for this project was necessary so that I can better understand the structure of the application and how to place information so users do not have to think before using it.",
+                "image": [
+                    "solution.webp",
+                    "solution.webp",
+                ]
+            },
+            "crafted solution": {
+                "desc": "To guide my design process, I developed personas representing both agents and customers. Understanding their needs, challenges, and aspirations was pivotal in shaping our user-centered approach.\n\nI had concentrated on the two sorts of Product Users, who make up the majority of the user base, for this early iteration. I combined the user profiles I built into a single persona, however, because the product is a single offering that is distinguished by responsibilities and permissions. Based on the UX research, the following persona exemplifies those roles and aids in keeping the design process user-centred:",
+                "image": [
+                    "testing.webp",
+                    "testing.webp",
+                    "testing.webp",
+                    "testing.webp",
+                    "testing.webp",
+                ],
+            },
+            "lessons learned": {
+                "parts": {
+                    "Community-Centric Approach": "Prioritizing local knowledge and relationships is key to fostering trust and adoption.",
+                    "Iterative Design Process": "Continuous testing and refinement are essential for meeting the evolving needs of users and stakeholders",
+                    "Simplicity Drives Adoption": "Designing for simplicity and ease of use is paramount, especially in communities with varying levels of digital literacy."
+                },
+            }
+        }
+    },
 }
 
-export default Data
+export default Data;
