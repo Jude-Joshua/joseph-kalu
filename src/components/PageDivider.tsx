d88b886c2d32a35df876bed2534dc2f98b53ec06
Joseph Kalu <mrjosephkalu@gmail.com>
2026-04-21 22:27:04 +0300

    chore: remove old Vite/React source files and duplicate project images

--- src/components/PageDivider.tsx
+++
@@ -1,9 +0,0 @@
-const PageDivider: React.FC = () => {
-    return (
-        <article className={'pages-divider flex flex-row'}>
-            <div className={'liner'}/>
-        </article>
-    )
-}
-
-export default PageDivider
